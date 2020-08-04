7a8f448aeeea758523da9491fa8dcade3db2a3e7
Owen <owen.le.goldenowl@gmail.com>
2020-08-04 20:34:08 +0700

    subscribe to changes of state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore"
 import TodosContainer from "./containers/TodosContainer"
+import { saveTodosToLocalStorage } from "./utils/todoUtils"
 import './App.css';
 
 const store = configureStore();
+
+store.subscribe(() => {
+  const { todos } = store.getState();
+  saveTodosToLocalStorage(todos.items)
+});
 
 class App extends Component {
   constructor(props) {
